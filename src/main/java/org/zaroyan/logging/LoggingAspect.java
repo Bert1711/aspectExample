f1387ec600174a599fde4f876eee12cf8011a0d7
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-04-02 22:41:41 +0300

    "add LoggingAspect"

--- src/main/java/org/zaroyan/logging/LoggingAspect.java
+++ src/main/java/org/zaroyan/logging/LoggingAspect.java
@@ -1,10 +1,20 @@
 package org.zaroyan.logging;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+
+import java.util.logging.Logger;
 
 @Aspect
 public class LoggingAspect {
-    public void log() {
-        // fhjdk
+    Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+
+    @Around("execution(* org.zaroyan.services.*.*(..))")
+    public void log(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info("До выполнения метода");
+        joinPoint.proceed();
+        logger.info("После выполнения метода");
+
     }
 }
